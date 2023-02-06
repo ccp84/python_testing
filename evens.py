90e484757ac3318b4d3da65dbdb32ac43890ac28
ccp84 <ccp84@icloud.com>
2023-02-06 12:15:18 +0000

    py>completed tdd

--- evens.py
+++ evens.py
@@ -1,6 +1,16 @@
 def even_number_of_evens(numbers):
     if isinstance(numbers, list):
-        return True
+            evens = 0
+            for number in numbers:
+                if number %2 == 0:
+                    evens += 1
+            if evens %2 == 0:
+                if evens == 0:
+                    return False
+                else:
+                    return True
+            else:
+                return False
     else:
         raise TypeError("Value should be a list")
 
--- test_evens.py
+++ test_evens.py
@@ -6,6 +6,14 @@ class TestEvens(unittest.TestCase):
 
     def test_throws_error_if_value_is_not_list(self):
         self.assertRaises(TypeError, even_number_of_evens, 4)
+    
+    def test_values_passed_in(self):
+        self.assertEqual(even_number_of_evens([]), False)
+        self.assertEqual(even_number_of_evens([2, 4]), True)
+        self.assertEqual(even_number_of_evens([2]), False)
+        self.assertEqual(even_number_of_evens([3]), False)
+        self.assertEqual(even_number_of_evens([3, 5]), False)
+        self.assertEqual(even_number_of_evens([3, 4]), False)
 
 
 if __name__ == "__main__":
